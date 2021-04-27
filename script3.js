fc18bdca4ecc2eabebab10aec5f29b05359c0cd5
Shahzad1414 <47620765+Shahzad1414@users.noreply.github.com>
2021-04-28 00:45:49 +0500

    Layout changes and all request done using ajax
    -Changes has been done student2

--- script3.js
+++ script3.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var __dom = document.getElementById("view2_barchart");
+    var __dom = document.getElementById("view3_barchart");
     var __myChart = echarts.init(__dom);
     var app = {};
 
@@ -7,32 +7,33 @@ $(document).ready(function() {
     $.ajax({
 
         dataType: "json",
-        url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/temperature",
+        url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction",
         type: 'GET',
         success: function(data) {
-            $("#content_1 #View2Tbl").empty();
-            $("#heading").text("The latest 20 measurements");
+            $("#view3_content_1 #View3Tbl").empty();
+            $("#heading").text("latest 20 measurements and Types");
+
             let tab =
                 `<tr>
-                    <th>Row_Number</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Measurement_Type</th>
-                    <th>Value</th>
-                </tr>`;
+                            <th>Row_Number</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Measurement_Type</th>
+                            <th>Value</th>
+                        </tr>`;
             let count = 0;
             for (let r of data) {
 
                 count = count + 1;
                 if (count <= 20) {
                     tab += `<tr> 
-                    <td>${count} </td>
-                    <td>${r.date_time.slice(0,10)}</td> 
-                    <td>${r.date_time.slice(11,19)}</td>
-                    <td>temperature</td>
-                    <td>${r.temperature}</td>
-                            
-                </tr>`;
+                            <td>${count} </td>
+                            <td>${r.date_time.slice(0,10)}</td> 
+                            <td>${r.date_time.slice(11,19)}</td>
+                            <td>wind_direction</td>
+                            <td>${r.wind_direction}</td>
+                                    
+                        </tr>`;
 
                     option = {
                         xAxis: {
@@ -44,7 +45,7 @@ $(document).ready(function() {
                             type: 'value'
                         },
                         series: [{
-                            data: data.map(item => item.temperature),
+                            data: data.map(item => item.wind_direction),
                             type: 'bar'
                         }]
                     };
@@ -56,23 +57,24 @@ $(document).ready(function() {
 
 
             }
-            tablebody = $("#content_1 #View2Tbl");
+            tablebody = $("#view3_content_1 #View3Tbl");
             tablebody.append(tab);
         }
     });
-    $('#timeinterval').change(function() {
-        var timeinterval = $('#timeinterval').find(":selected").text();
+    $('#view3_timeinterval').change(function() {
+        var view3_timeinterval = $('#view3_timeinterval').find(":selected").text();
 
-        if (timeinterval == "Now") {
+        if (view3_timeinterval == "Now") {
 
             $.ajax({
 
                 dataType: "json",
-                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/temperature",
+                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction",
                 type: 'GET',
                 success: function(data) {
-                    $("#content_1 #View2Tbl").empty();
-                    $("#heading").text("The latest 20 measurements");
+                    $("#view3_content_1 #View3Tbl").empty();
+                    $("#heading").text("latest 20 measurments an types");
+
                     let tab =
                         `<tr>
                                     <th>Row_Number</th>
@@ -90,8 +92,8 @@ $(document).ready(function() {
                                     <td>${count} </td>
                                     <td>${r.date_time.slice(0,10)}</td> 
                                     <td>${r.date_time.slice(11,19)}</td>
-                                    <td>temperature</td>
-                                    <td>${r.temperature}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
                                             
                                 </tr>`;
 
@@ -105,7 +107,7 @@ $(document).ready(function() {
                                     type: 'value'
                                 },
                                 series: [{
-                                    data: data.map(item => item.temperature),
+                                    data: data.map(item => item.wind_direction),
                                     type: 'bar'
                                 }]
                             };
@@ -117,20 +119,21 @@ $(document).ready(function() {
 
 
                     }
-                    tablebody = $("#content_1 #View2Tbl");
+                    tablebody = $("#view3_content_1 #View3Tbl");
                     tablebody.append(tab);
                 }
             });
         } else
-        if (timeinterval == "24 hours") {
+        if (view3_timeinterval == "24 hours") {
             $.ajax({
 
                 dataType: "json",
-                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/temperature/23",
+                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction/23",
                 type: 'GET',
                 success: function(data) {
-                    $("#content_1 #View2Tbl").empty();
+                    $("#view3_content_1 #View3Tbl").empty();
                     $("#heading").text("latest 24 hours, values are hourly averages");
+
                     let tab =
                         `<tr>
                                     <th>Row_Number</th>
@@ -148,8 +151,8 @@ $(document).ready(function() {
                                     <td>${count} </td>
                                     <td>${r.date_time.slice(0,10)}</td> 
                                     <td>${r.date_time.slice(11,19)}</td>
-                                    <td>temperature</td>
-                                    <td>${r.temperature}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
                                             
                                 </tr>`;
                         option = {
@@ -162,7 +165,7 @@ $(document).ready(function() {
                                 type: 'value'
                             },
                             series: [{
-                                data: data.map(item => item.temperature),
+                                data: data.map(item => item.wind_direction),
                                 type: 'bar'
                             }]
                         };
@@ -174,20 +177,20 @@ $(document).ready(function() {
 
 
                     }
-                    tablebody = $("#content_1 #View2Tbl");
+                    tablebody = $("#view3_content_1 #View3Tbl");
                     tablebody.append(tab);
                 }
             });
         } else
 
-        if (timeinterval == "48 hours") {
+        if (view3_timeinterval == "48 hours") {
             $.ajax({
 
                 dataType: "json",
-                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/temperature/47",
+                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction/47",
                 type: 'GET',
                 success: function(data) {
-                    $("#content_1 #View2Tbl").empty();
+                    $("#view3_content_1 #View3Tbl").empty();
                     $("#heading").text("latest 48 hours, values are hourly averages");
 
                     let tab =
@@ -207,8 +210,8 @@ $(document).ready(function() {
                                     <td>${count} </td>
                                     <td>${r.date_time.slice(0,10)}</td> 
                                     <td>${r.date_time.slice(11,19)}</td>
-                                    <td>temperature</td>
-                                    <td>${r.temperature}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
                                             
                                 </tr>`;
                         option = {
@@ -221,7 +224,7 @@ $(document).ready(function() {
                                 type: 'value'
                             },
                             series: [{
-                                data: data.map(item => item.temperature),
+                                data: data.map(item => item.wind_direction),
                                 type: 'bar'
                             }]
                         };
@@ -233,21 +236,22 @@ $(document).ready(function() {
 
 
                     }
-                    tablebody = $("#content_1 #View2Tbl");
+                    tablebody = $("#view3_content_1 #View3Tbl");
                     tablebody.append(tab);
                 }
             });
         } else
 
-        if (timeinterval == "72 hours") {
+        if (view3_timeinterval == "72 hours") {
             $.ajax({
 
                 dataType: "json",
-                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/temperature/71",
+                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction/71",
                 type: 'GET',
                 success: function(data) {
-                    $("#content_1 #View2Tbl").empty();
+                    $("#view3_content_1 #View3Tbl").empty();
                     $("#heading").text("latest 72 hours, values are hourly averages");
+
 
                     let tab =
                         `<tr>
@@ -266,8 +270,8 @@ $(document).ready(function() {
                                     <td>${count} </td>
                                     <td>${r.date_time.slice(0,10)}</td> 
                                     <td>${r.date_time.slice(11,19)}</td>
-                                    <td>temperature</td>
-                                    <td>${r.temperature}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
                                             
                                 </tr>`;
                         option = {
@@ -280,7 +284,7 @@ $(document).ready(function() {
                                 type: 'value'
                             },
                             series: [{
-                                data: data.map(item => item.temperature),
+                                data: data.map(item => item.wind_direction),
                                 type: 'bar'
                             }]
                         };
@@ -292,20 +296,20 @@ $(document).ready(function() {
 
 
                     }
-                    tablebody = $("#content_1 #View2Tbl");
+                    tablebody = $("#view3_content_1 #View3Tbl");
                     tablebody.append(tab);
                 }
             });
         } else
 
-        if (timeinterval == "1 week") {
+        if (view3_timeinterval == "1 week") {
             $.ajax({
 
                 dataType: "json",
-                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/temperature/167",
+                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction/167",
                 type: 'GET',
                 success: function(data) {
-                    $("#content_1 #View2Tbl").empty();
+                    $("#view3_content_1 #View3Tbl").empty();
                     $("#heading").text("latest week, values are hourly averages");
 
                     let tab =
@@ -325,8 +329,8 @@ $(document).ready(function() {
                                     <td>${count} </td>
                                     <td>${r.date_time.slice(0,10)}</td> 
                                     <td>${r.date_time.slice(11,19)}</td>
-                                    <td>temperature</td>
-                                    <td>${r.temperature}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
                                             
                                 </tr>`;
 
@@ -340,7 +344,7 @@ $(document).ready(function() {
                                 type: 'value'
                             },
                             series: [{
-                                data: data.map(item => item.temperature),
+                                data: data.map(item => item.wind_direction),
                                 type: 'bar'
                             }]
                         };
@@ -351,20 +355,20 @@ $(document).ready(function() {
 
 
                     }
-                    tablebody = $("#content_1 #View2Tbl");
+                    tablebody = $("#view3_content_1 #View3Tbl");
                     tablebody.append(tab);
                 }
             });
         } else
 
-        if (timeinterval == "1 month") {
+        if (view3_timeinterval == "1 month") {
             $.ajax({
 
                 dataType: "json",
-                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/temperature/730",
+                url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction/730",
                 type: 'GET',
                 success: function(data) {
-                    $("#content_1 #View2Tbl").empty();
+                    $("#view3_content_1 #View3Tbl").empty();
                     $("#heading").text("latest month, values are hourly averages");
 
                     let tab =
@@ -384,8 +388,8 @@ $(document).ready(function() {
                                     <td>${count} </td>
                                     <td>${r.date_time.slice(0,10)}</td> 
                                     <td>${r.date_time.slice(11,19)}</td>
-                                    <td>temperature</td>
-                                    <td>${r.temperature}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
                                             
                                 </tr>`;
 
@@ -399,7 +403,7 @@ $(document).ready(function() {
                                 type: 'value'
                             },
                             series: [{
-                                data: data.map(item => item.temperature),
+                                data: data.map(item => item.wind_direction),
                                 type: 'bar'
                             }]
                         };
@@ -410,7 +414,7 @@ $(document).ready(function() {
 
 
                     }
-                    tablebody = $("#content_1 #View2Tbl");
+                    tablebody = $("#view3_content_1 #View3Tbl");
                     tablebody.append(tab);
                 }
             });
