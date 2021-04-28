87239df5db81b00f49056b11d3f3637f982d687e
Shahzad1414 <47620765+Shahzad1414@users.noreply.github.com>
2021-04-29 02:27:41 +0500

    sorting and wind direction in view 4

--- script4.js
+++ script4.js
@@ -1506,6 +1506,365 @@ $(document).ready(function() {
                     });
                 }
             })
+        } else if (measurments == "wind_direction") {
+            $('#view4_timeinterval').change(function() {
+                var view4_timeinterval = $('#view4_timeinterval').find(":selected").text();
+
+                if (view4_timeinterval == "Now") {
+
+                    $.ajax({
+
+                        dataType: "json",
+                        url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction",
+                        type: 'GET',
+                        success: function(data) {
+                            $("#view4_content_1 #View4Tbl").empty();
+                            $("#heading").text("latest 50 measurments and Type");
+
+                            let tab =
+                                `<tr>
+                                    <th>Row_Number</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Measurement_Type</th>
+                                    <th>Value</th>
+                                </tr>`;
+                            let count = 0;
+                            for (let r of data) {
+
+                                count = count + 1;
+                                if (count <= 50) {
+                                    tab += `<tr> 
+                                    <td>${count} </td>
+                                    <td>${r.date_time.slice(0,10)}</td> 
+                                    <td>${r.date_time.slice(11,19)}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
+                                            
+                                </tr>`;
+
+                                    option = {
+                                        xAxis: {
+                                            type: 'category',
+                                            data: data.map(item => item.date_time.slice(11, 19))
+                                                // [r.date_time.slice(11, 19)]
+                                        },
+                                        yAxis: {
+                                            type: 'value'
+                                        },
+                                        series: [{
+                                            data: data.map(item => item.wind_direction),
+                                            type: 'line'
+                                        }]
+                                    };
+
+                                    if (option && typeof option === 'object') {
+                                        __myChart.setOption(option);
+                                    }
+                                }
+
+
+                            }
+                            tablebody = $("#view4_content_1 #View4Tbl");
+                            tablebody.append(tab);
+                        }
+                    });
+                } else
+                if (view4_timeinterval == "24 hours") {
+                    $.ajax({
+
+                        dataType: "json",
+                        url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction/23",
+                        type: 'GET',
+                        success: function(data) {
+                            $("#view4_content_1 #View4Tbl").empty();
+                            $("#heading").text("latest 24 hours, values are hourly averages");
+
+                            let tab =
+                                `<tr>
+                                    <th>Row_Number</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Measurement_Type</th>
+                                    <th>Value</th>
+                                </tr>`;
+                            let count = 0;
+                            for (let r of data) {
+
+                                count = count + 1;
+
+                                tab += `<tr> 
+                                    <td>${count} </td>
+                                    <td>${r.date_time.slice(0,10)}</td> 
+                                    <td>${r.date_time.slice(11,19)}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
+                                            
+                                </tr>`;
+                                option = {
+                                    xAxis: {
+                                        type: 'category',
+                                        data: data.map(item => item.date_time.slice(11, 19))
+                                            // [r.date_time.slice(11, 19)]
+                                    },
+                                    yAxis: {
+                                        type: 'value'
+                                    },
+                                    series: [{
+                                        data: data.map(item => item.wind_direction),
+                                        type: 'line'
+                                    }]
+                                };
+
+                                if (option && typeof option === 'object') {
+                                    __myChart.setOption(option);
+                                }
+
+
+
+                            }
+                            tablebody = $("#view4_content_1 #View4Tbl");
+                            tablebody.append(tab);
+                        }
+                    });
+                } else
+
+                if (view4_timeinterval == "48 hours") {
+                    $.ajax({
+
+                        dataType: "json",
+                        url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction/47",
+                        type: 'GET',
+                        success: function(data) {
+                            $("#view4_content_1 #View4Tbl").empty();
+                            $("#heading").text("latest 48 hours, values are hourly averages");
+
+                            let tab =
+                                `<tr>
+                                    <th>Row_Number</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Measurement_Type</th>
+                                    <th>Value</th>
+                                </tr>`;
+                            let count = 0;
+                            for (let r of data) {
+
+                                count = count + 1;
+
+                                tab += `<tr> 
+                                    <td>${count} </td>
+                                    <td>${r.date_time.slice(0,10)}</td> 
+                                    <td>${r.date_time.slice(11,19)}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
+                                            
+                                </tr>`;
+                                option = {
+                                    xAxis: {
+                                        type: 'category',
+                                        data: data.map(item => item.date_time.slice(11, 19))
+                                            // [r.date_time.slice(11, 19)]
+                                    },
+                                    yAxis: {
+                                        type: 'value'
+                                    },
+                                    series: [{
+                                        data: data.map(item => item.wind_direction),
+                                        type: 'line'
+                                    }]
+                                };
+
+                                if (option && typeof option === 'object') {
+                                    __myChart.setOption(option);
+                                }
+
+
+
+                            }
+                            tablebody = $("#view4_content_1 #View4Tbl");
+                            tablebody.append(tab);
+                        }
+                    });
+                } else
+
+                if (view4_timeinterval == "72 hours") {
+                    $.ajax({
+
+                        dataType: "json",
+                        url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction/71",
+                        type: 'GET',
+                        success: function(data) {
+                            $("#view4_content_1 #View4Tbl").empty();
+                            $("#heading").text("latest 72 hours, values are hourly averages");
+
+                            let tab =
+                                `<tr>
+                                    <th>Row_Number</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Measurement_Type</th>
+                                    <th>Value</th>
+                                </tr>`;
+                            let count = 0;
+                            for (let r of data) {
+
+                                count = count + 1;
+
+                                tab += `<tr> 
+                                    <td>${count} </td>
+                                    <td>${r.date_time.slice(0,10)}</td> 
+                                    <td>${r.date_time.slice(11,19)}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
+                                            
+                                </tr>`;
+                                option = {
+                                    xAxis: {
+                                        type: 'category',
+                                        data: data.map(item => item.date_time.slice(11, 19))
+                                            // [r.date_time.slice(11, 19)]
+                                    },
+                                    yAxis: {
+                                        type: 'value'
+                                    },
+                                    series: [{
+                                        data: data.map(item => item.wind_direction),
+                                        type: 'line'
+                                    }]
+                                };
+
+                                if (option && typeof option === 'object') {
+                                    __myChart.setOption(option);
+                                }
+
+
+
+                            }
+                            tablebody = $("#view4_content_1 #View4Tbl");
+                            tablebody.append(tab);
+                        }
+                    });
+                } else
+
+                if (view4_timeinterval == "1 week") {
+                    $.ajax({
+
+                        dataType: "json",
+                        url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction/167",
+                        type: 'GET',
+                        success: function(data) {
+                            $("#view4_content_1 #View4Tbl").empty();
+                            $("#heading").text("latest week, values are hourly averages");
+
+                            let tab =
+                                `<tr>
+                                    <th>Row_Number</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Measurement_Type</th>
+                                    <th>Value</th>
+                                </tr>`;
+                            let count = 0;
+                            for (let r of data) {
+
+                                count = count + 1;
+
+                                tab += `<tr> 
+                                    <td>${count} </td>
+                                    <td>${r.date_time.slice(0,10)}</td> 
+                                    <td>${r.date_time.slice(11,19)}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
+                                            
+                                </tr>`;
+
+                                option = {
+                                    xAxis: {
+                                        type: 'category',
+                                        data: data.map(item => item.date_time.slice(11, 19))
+                                            // [r.date_time.slice(11, 19)]
+                                    },
+                                    yAxis: {
+                                        type: 'value'
+                                    },
+                                    series: [{
+                                        data: data.map(item => item.wind_direction),
+                                        type: 'line'
+                                    }]
+                                };
+
+                                if (option && typeof option === 'object') {
+                                    __myChart.setOption(option);
+                                }
+
+
+                            }
+                            tablebody = $("#view4_content_1 #View4Tbl");
+                            tablebody.append(tab);
+                        }
+                    });
+                } else
+
+                if (view4_timeinterval == "1 month") {
+                    $.ajax({
+
+                        dataType: "json",
+                        url: "http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction/730",
+                        type: 'GET',
+                        success: function(data) {
+                            $("#view4_content_1 #View4Tbl").empty();
+                            $("#heading").text("latest month, values are hourly averages");
+
+                            let tab =
+                                `<tr>
+                                    <th>Row_Number</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Measurement_Type</th>
+                                    <th>Value</th>
+                                </tr>`;
+                            let count = 0;
+                            for (let r of data) {
+
+                                count = count + 1;
+
+                                tab += `<tr> 
+                                    <td>${count} </td>
+                                    <td>${r.date_time.slice(0,10)}</td> 
+                                    <td>${r.date_time.slice(11,19)}</td>
+                                    <td>wind_direction</td>
+                                    <td>${r.wind_direction}</td>
+                                            
+                                </tr>`;
+
+                                option = {
+                                    xAxis: {
+                                        type: 'category',
+                                        data: data.map(item => item.date_time.slice(11, 19))
+                                            // [r.date_time.slice(11, 19)]
+                                    },
+                                    yAxis: {
+                                        type: 'value'
+                                    },
+                                    series: [{
+                                        data: data.map(item => item.wind_direction),
+                                        type: 'line'
+                                    }]
+                                };
+
+                                if (option && typeof option === 'object') {
+                                    __myChart.setOption(option);
+                                }
+
+
+                            }
+                            tablebody = $("#view4_content_1 #View4Tbl");
+                            tablebody.append(tab);
+                        }
+                    });
+                }
+            })
         }
     })
 })
